66998955d21edca6c425b80c0de47148bf95e460
GeorgianaDeleaSarca <g.delea.sarca@gmail.com>
2021-01-02 19:52:31 +0200

    Add project files.

---
+++ Data/DeleaSarca_Georgiana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DeleaSarca_Georgiana_Lab8.Models;
+
+namespace DeleaSarca_Georgiana_Lab8.Data
+{
+    public class DeleaSarca_Georgiana_Lab8Context : DbContext
+    {
+        public DeleaSarca_Georgiana_Lab8Context (DbContextOptions<DeleaSarca_Georgiana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DeleaSarca_Georgiana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<DeleaSarca_Georgiana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<DeleaSarca_Georgiana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DeleaSarca_Georgiana_Lab8.Data;
+using DeleaSarca_Georgiana_Lab8.Models;
+
+namespace DeleaSarca_Georgiana_Lab8.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DeleaSarca_Georgiana_Lab8.Data.DeleaSarca_Georgiana_Lab8Context _context;
+
+        public CreateModel(DeleaSarca_Georgiana_Lab8.Data.DeleaSarca_Georgiana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
